98947db6eff443d9021dd4bb8fee61b25ae6e280
Mihael Savin <jtprogru@gmail.com>
2020-07-03 01:35:58 +0300

    Понятия не имею что я тут делал;

--- main.py
+++ main.py
@@ -9,40 +9,24 @@ __version__ = '0.0.1'
 __author_email__ = 'mail@jtprog.ru'
 
 import requests
-import hashlib
-from time import sleep
-
-base_url = 'https://habr.com/ru/post/'
-url_list = []
-itr = 0
+from fake_useragent import UserAgent
+from bs4 import BeautifulSoup
 
 
-def check_url(url):
-    try:
-        sleep(0.5)
-        r = requests.head(url, proxies={'https': 'https://178.16.152.254:3128'})
-        if r.status_code == 200:
-            return True
-        else:
-            return False
-    except requests.exceptions.ConnectionError:
-        pass
+base_url = 'https://python-scripts.com/requests'
 
 
-def save_url(urll):
-    with open('data/url_list.txt', 'a') as f:
-        f.writelines(f"{urll}\n")
+try:
+    r = requests.head(base_url,
+                      proxies={'https': 'https://178.16.152.254:3128'},
+                      headers={'User-Agent': UserAgent().chrome})
+    if r.status_code == 200:
+        soup = BeautifulSoup(r.text, 'lxml')
 
 
-for i in range(472454, 473454):
-    url_ = base_url + str(i) + '/'
-    if check_url(url_):
-        itr += 1
-        url_list.append(url_)
-        h = hashlib.md5(url_.encode('utf-8')).hexdigest()
-        fstr = f'[{itr}] URL: {url_} \n\tHASH: {h}'
-        save_url(fstr)
+except requests.exceptions.ConnectionError:
+    pass
 
-print(len(url_list))
+
 
 
